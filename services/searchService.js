b7ae04c7e70376178aa46efe04e846f38c3d1a98
Lakshay Setia <lakshaysetia123@gmail.com>
2023-06-13 00:35:28 +0530

    added search by name

--- services/searchService.js
+++ services/searchService.js
@@ -4,7 +4,7 @@ const getSeriesFromQuery = async (query) => {
   const url =
     "https://api.themoviedb.org/3/search/tv?api_key=eaa42a85812c87f3123038ef1c44278e&query=" +
     query +
-    "jack&include_adult=true&language=en-US&page=1";
+    "&include_adult=true&language=en-US&page=1";
   const response = await axios.get(url);
   return response;
 };
